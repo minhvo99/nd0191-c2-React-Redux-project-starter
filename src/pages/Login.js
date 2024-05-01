f63e673d4b7ff2abf669bfe2916e6097fb8d246a
Minh Vo <pikachu43dn@gmail.com>
2024-05-01 18:53:57 +0700

    update test case

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -75,12 +75,13 @@ const Login = ({ dispatch, loggedIn }) => {
           </ul>
         </div>
         {/* Login Form with username and password */}
-        <form
-          onSubmit={handleSubmit}
-          className=" form-control w-full max-w-lg space-y-6"
-        >
-          <div className=" space-y-2">
-            <label data-testid="username-label" htmlFor="username">
+        <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
+          <div className="mb-5">
+            <label
+              htmlFor="username"
+              data-testid="username-label"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
               Username
             </label>
             <input
@@ -92,18 +93,24 @@ const Login = ({ dispatch, loggedIn }) => {
                 })
               }
               value={credentials.username}
-              className="input input-bordered w-full max-w-lg"
               type="text"
               id="username"
-              name="username"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+
             />
           </div>
-
-          <div className=" space-y-2">
-            <label data-testid="password-label" htmlFor="password">
+          <div className="mb-5">
+            <label
+              data-testid="password-label"
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
               Password
             </label>
             <input
+              type="password"
+              id="password"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
               data-testid="password-input"
               onChange={(e) =>
                 setCredentials({
@@ -112,23 +119,22 @@ const Login = ({ dispatch, loggedIn }) => {
                 })
               }
               value={credentials.password}
-              className="input input-bordered w-full max-w-lg"
-              type="password"
-              id="password"
-              name="password"
             />
           </div>
-          {error && (
-          <div className="alert alert-error shadow-lg max-w-lg">
-            <div>
-              <i className="fa-regular fa-circle-xmark"></i>
-              <span data-testid="error-message">
-                Username or password is incorrect
-              </span>
-            </div>
-          </div>
-        )}
-          <button data-testid="submit-login" className="btn">
+            {error && (
+              <div className="alert alert-error">
+                <div>
+                  <i className="fa-regular fa-circle-xmark"></i>
+                  <span data-testid="error-message">
+                    Username or password is incorrect
+                  </span>
+                </div>
+              </div>
+            )}
+          <button
+            data-testid="submit-login"
+            className="btn text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
             Login
           </button>
         </form>
